5c81053a46e99656b91d55713609407258bf2b5d
Max Miller <104030734+Litigator98765@users.noreply.github.com>
2023-07-31 16:02:01 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,42 @@
-import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.select import By
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-URL = "https://www.opentable.com/state-of-industry"
+url = 'https://www.opentable.com/state-of-industry'
 
-headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
+#Get DataFrame
+dfs = pd.read_html(url)
 
-try: 
-    page = requests.get(URL, allow_redirects=False, timeout=10, headers = headers)
-except requests.exceptions.Timeout as err: 
-    print(err)
+#Get first DataFrame
+df = dfs[0]
+df.to_excel('OpenTable/country.xlsx', index=False)
 
-print(page.text)
+## create an object of the chrome webdriver
+driver = webdriver.Chrome()
+## open selenium URL in chrome browser
+driver.get('https://www.opentable.com/state-of-industry')
+
+#Switch to state table
+select = Select(driver.find_element(By.CLASS_NAME, "x3G8ivweF9MW6w3zqa6N"))
+select.select_by_value('states')
+#Get state table data and write to excel
+dfs = pd.read_html(driver.page_source)
+df = dfs[0]
+df.to_excel('OpenTable/state.xlsx', index=False)
+
+
+#Switch to city table
+select = Select(driver.find_element(By.CLASS_NAME, "x3G8ivweF9MW6w3zqa6N"))
+select.select_by_value('cities')
+#Get city table and write to excel file
+dfs = pd.read_html(driver.page_source)
+df = dfs[0]
+df.to_excel('OpenTable/city.xlsx', index=False)
+
+#close browser
+driver.close()
+
