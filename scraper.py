4ef1a6a9bfca453293cb09552b7117bfadf62ca2
Max Miller <104030734+Litigator98765@users.noreply.github.com>
2023-08-09 14:07:35 -0400

    Appends entries now

--- scraper.py
+++ scraper.py
@@ -2,41 +2,50 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.select import By
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
 
 url = 'https://www.opentable.com/state-of-industry'
 
-#Get DataFrame
+#Get DataFrame Set
 dfs = pd.read_html(url)
 
-#Get first DataFrame
+#Get COUNTRY DataFrame and write to excel
 df = dfs[0]
-df.to_excel('OpenTable/country.xlsx', index=False)
+fileName = 'OpenTable/country.xlsx'
+writer = pd.ExcelWriter(fileName, engine='openpyxl', mode='a', if_sheet_exists='overlay')
+df.to_excel(writer, index=False)
+#Close writer
+writer.close()
 
-## create an object of the chrome webdriver
+#Create an object of the chrome webdriver
 driver = webdriver.Chrome()
-## open selenium URL in chrome browser
+#Open selenium URL in chrome browser
 driver.get('https://www.opentable.com/state-of-industry')
 
-#Switch to state table
+#Select STATE table using class value. ?!COULD CHANGE!?
 select = Select(driver.find_element(By.CLASS_NAME, "x3G8ivweF9MW6w3zqa6N"))
 select.select_by_value('states')
-#Get state table data and write to excel
+#Get state DataFrame and write to excel
 dfs = pd.read_html(driver.page_source)
 df = dfs[0]
-df.to_excel('OpenTable/state.xlsx', index=False)
+fileName = 'OpenTable/state.xlsx'
+writer = pd.ExcelWriter(fileName, engine='openpyxl', mode='a', if_sheet_exists='overlay')
+df.to_excel(writer, index=False)
+#Close writer
+writer.close()
 
-
-#Switch to city table
+#Select CITY table using class value. ?!COULD CHANGE!?
 select = Select(driver.find_element(By.CLASS_NAME, "x3G8ivweF9MW6w3zqa6N"))
 select.select_by_value('cities')
 #Get city table and write to excel file
 dfs = pd.read_html(driver.page_source)
 df = dfs[0]
-df.to_excel('OpenTable/city.xlsx', index=False)
+fileName = 'OpenTable/city.xlsx'
+writer = pd.ExcelWriter(fileName, engine='openpyxl', mode='a', if_sheet_exists='overlay')
+df.to_excel(writer, index=False)
+#Close writer
+writer.close()
 
-#close browser
+#Close browser
 driver.close()
+
 
